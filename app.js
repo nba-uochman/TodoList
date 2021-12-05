088bfec387e3f8b48a074b597ff994df884c9eca
uche patrick <uchepatric04@gmail.com>
2021-12-05 04:50:44 +0100

    add git ignore and procfile

--- app.js
+++ app.js
@@ -148,6 +148,6 @@ app.get("/:customName", function (req, res) {
 
 
 
-app.listen(3000, function () {
-    console.log("server running on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server running successfully");
 });
