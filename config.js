75dc0d9fae81f70c072c86463659f4b3b054f657
nba-uochman <uchepatric04@gmail.com>
2022-01-31 23:36:40 +0100

    update app

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
 const config = {
-    connection: "mongodb+srv://admin-uche:nbauochman@cluster0.n7gta.mongodb.net/todoListDB"
+    connection: "mongodb+srv://admin-uche:uche123456@cluster0.n7gta.mongodb.net/todoListDB"
 }
+
+// nbauochman
 
 module.exports = config;
